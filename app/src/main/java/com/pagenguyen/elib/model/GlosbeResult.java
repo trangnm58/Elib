2c21ae215693587b879f1ee100e63477ec0bab4f
Nguyễn Minh Trang <pagenguyen219@gmail.com>
2015-12-05 15:15:29 +0700

    fix to use english examples as well
    mExamples in GlosbeResult contains english examples in its even indice
    (0,2,4) and vietnamese examples in its odd indice (1,3,5)

--- app/src/main/java/com/pagenguyen/elib/model/GlosbeResult.java
+++ app/src/main/java/com/pagenguyen/elib/model/GlosbeResult.java
@@ -4,8 +4,8 @@ package com.pagenguyen.elib.model;
  * Created by Page on 02/12/2015.
  */
 public class GlosbeResult {
-	private String[] mTranslations;
-	private String[] mExamples;
+	private String[] mTranslations; // array of meanings
+	private String[] mExamples; // array of english - vietnamese examples
 
 	public String[] getTranslations() {
 		return mTranslations;
